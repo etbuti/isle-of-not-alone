3fa96a1815f9ea78179cc1fac14f148d82f33353
etbuti <156723667+etbuti@users.noreply.github.com>
2025-06-29 12:32:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,32 @@
 self.addEventListener('install', event => {
   console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open('static-v1').then(cache =>
+      cache.addAll([
+        '/',                   // 首页
+        '/index.html',         // 入口文件
+        '/manifest.json',      // manifest
+        '/icons/icon-192.png', // 小图标
+        '/icons/icon-512.png', // 大图标
+        // 如果有CSS或JS文件也可以加上，比如：
+        // '/styles.css',
+        // '/app.js',
+      ])
+    )
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   console.log('Service Worker activating.');
+  // 清理旧缓存（可选）
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== 'static-v1').map(key => caches.delete(key))
+      )
+    )
+  );
 });
 
 self.addEventListener('fetch', event => {
