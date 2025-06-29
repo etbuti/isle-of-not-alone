9652f36d9942b0c49afb51da1412b26e6d6f2be5
etbuti <156723667+etbuti@users.noreply.github.com>
2025-06-29 13:26:47 +0100

    Update service-worker.js
    Add basic service worker

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,16 @@
-const CACHE_NAME = 'goldisle-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/budcup2025.html',
-  '/budcup2025-cn.html',
-  '/music.html',
-  '/trade.html',
-  // 如果你还有其他页面或资源文件，如 CSS、图片等，也可以在这里继续添加
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log('Service Worker installing.');
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-                  .map(name => caches.delete(name))
-      );
-    })
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
+});
+
   );
 });
 
